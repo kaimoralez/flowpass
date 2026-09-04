3e5ad9ee5d0e66fe070fd3ce97840b8ad0c59c8d
kaimoralez <kapconc2009@gmail.com>
2026-09-04 11:35:52 -0300

    feat: add header component

--- flowpass-frontend/src/layout/header/header.jsx
+++ flowpass-frontend/src/layout/header/header.jsx
@@ -1,12 +1,13 @@
+import "./style.css"
 import { Button } from "../../components/Button/button"
+
 
 export const Header = () =>{
     return(
         <header>
-            <h1>FLOWPASS</h1>
+            <h1 className="logo-text">FLOWPASS</h1>
             <Button texto={"+ Novo Pedido"} variant={"primary"} size={"short"}></Button>
         </header>
-        
-
+    
     )
 }
